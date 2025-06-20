5b8ad94dcc02f6e7816843f045f1ae492289475f
Benjamin C Hansen <31937992+benconomics@users.noreply.github.com>
2025-06-20 15:42:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // Trail options by system
   const trailOptions = {
     "Carpenter Bypass": ["Alpha", "B-Line", "Bloody Stump", "Boneyard", "Boundary", "Brad's Bad Gravity", "Burial Ground", "Don Sled", "Egress", "Eric's Drop", "Fun Times", "He", "It'll be Great", "Letz Peak","Lollipop","Mama Tried", "Meth Lab", "Micronet", "More Play", "Moto Alley", "Moto Alley", "Northridge", "NRA", "Ode to Joy", "Salal", "Snakepath", "SST", "Stumps Don't Win", "Sunset","Tree Prison", "Uno Mas", "Whole Nine Yards", "Y2k"],
-    "Thurston": ["Dead Mountain", "Lawler", "Hardesty", "Larison Rock"],
+    "Thurston": ["Acer Spades", "Yew Haw", "Super Maple", "Chinquapin Chutes"],
     "Ridgeline": ["Baby Steps", "Pipedream"],
     "Hardesty": ["Goodman", "Hardesty", "Willamette"]
   };
